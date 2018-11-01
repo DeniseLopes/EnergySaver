43898d52c7541d8b27af7a14f745ac14dec274b3
DIEGO <diegommorais12@gmail.com>
2018-10-31 22:22:00 -0200

    adicionando footer

--- view/templates/footerLogado.php
+++ view/templates/footerLogado.php
@@ -1,4 +1,10 @@
+
 </div>
+
+</div>
+
+
+
 <!-- page-wrapper -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
@@ -8,5 +14,10 @@ crossorigin="anonymous"></script>
 <script type="text/javascript" src="../../assets/js/script.js"></script>
 
 </body>
+<footer>
+  <div class="container">
+    <p>Put together in less than five minutes by <a href="http://www.martinbean.co.uk/" rel="author">Martin Bean</a>. Uses <a href="http://twitter.github.com/bootstrap/" rel="external">Twitter Bootstrap</a> and <a href="http://ryanfait.com/sticky-footer/" rel="external">Ryan Fait’s Sticky Footer</a> and <a href="#">Aalaap Ghag’s Responsive Padding Fix</a>.</p>
+  </div>
+</footer>
 
 </html>
