e6aa21e481a62d6a2dd7cf7d72d3ed76bc47583f
Magnomo <diegommorais12@gmail.com>
2018-11-13 16:38:30 -0200

    Add files via upload

--- assets/js/graficos.js
+++ assets/js/graficos.js
@@ -1,50 +1,4 @@
-	var ctx = $("#myChart");
-	var myChart = new Chart(ctx, {
-		type: 'line',
-		data: {
-			labels: ["00:00", "02:00", "04:00","06:00", "08:00","10:00", "12:00","14:00", "16:00","18:00", "20:00","22:00", "23:59"],
-			datasets: [{
-				label: 'Registro do dia 07/11/2018',
-				data: [0, 6, 15,12, 3,5, 18,14, 10,19, 11,5,10],
-				backgroundColor: [
-				'rgba(69, 69, 69, 0.2)'
-				
-				],
-				borderColor: [
-				'rgba(0,0,0,1)'
-				
-				],
-				borderWidth: 1
-			},{
-				label: 'Registro do dia 09/11/2018',
-				data: [0, 3, 12,2, 13,5, 8,14, 15,10, 16,15,0],
-				backgroundColor: [
-				'rgba(90, 1173, 173, 0.2)'
-				
-				],
-				borderColor: [
-				'rgba(64, 128, 128, 1)'
-				
-				],
-				borderWidth: 1
 
-			},
-			]
-		},
-		options:{
-			title:{
-				display:true,
-				fontSize:20,
-				text: "Grafico de consumo "
-			},
-			labels:{
-				fontStyle:"bold"
-			}
-			
-		}
-
-		
-	});
 	$(document).ready(function(){
 		$('[data-toggle="tooltip"]').tooltip();
 			var valor = $('#ss').val();
@@ -61,7 +15,7 @@
 				options+= '<option value="'+ valor['id'] + '">'+valor['modelo'] +"</option>";
 				$('#ss').html(options);
 			});
-	/*		console.log(options);*/
+			console.log(options);
 		}).fail(function(){
 			console.log("fail");
 		});
